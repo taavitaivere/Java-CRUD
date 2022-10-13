b1f466c07839572c31dc1c5e02176f46f7367eb2
taavitaivere <taavi@play.ee>
2022-10-13 23:25:04 +0300

    Add new user functionality

--- src/main/java/com/mycompany/user/UserController.java
+++ src/main/java/com/mycompany/user/UserController.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 
@@ -24,5 +25,12 @@ public class UserController {
         User user = new User();
         model.addAttribute("user", user);
         return "user_form";
+    }
+
+    @PostMapping("/users/save")
+    public String saveUser(User user) {
+        service.save(user);
+
+        return "redirect:/users";
     }
 }
