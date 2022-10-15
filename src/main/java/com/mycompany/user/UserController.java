43918a671acd4c358c5d04dc2636b4d0899582f2
taavitaivere <taavi@play.ee>
2022-10-15 12:35:12 +0300

    Delete user functionality

--- src/main/java/com/mycompany/user/UserController.java
+++ src/main/java/com/mycompany/user/UserController.java
@@ -49,4 +49,15 @@ public class UserController {
             return "redirect:/users";
         }
     }
+
+    @GetMapping("/users/delete/{id}")
+    public String deleteUser(@PathVariable("id") Integer id, RedirectAttributes ra) {
+        try {
+            service.delete(id);
+            ra.addFlashAttribute("message", "The user ID " + id + " has been deleted successfully.");
+        } catch (UserNotFoundException ex) {
+            ra.addFlashAttribute("message", ex.getMessage());
+        }
+        return "redirect:/users";
+    }
 }
